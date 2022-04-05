61795812117cebbf764b794abafd4baad496bb22
kyle.macasinag <kylenmac@verizon.net>
2022-04-05 16:41:27 -0400

    Weapon selection mostly implemented. Figuring out how to make it work

--- robot.py
+++ robot.py
@@ -6,8 +6,10 @@ class Robot:
     def __init__(self, name):
         self.robot_name = name
         self.robot_health = 100
-        self.active_weapon = Weapon("Flamer", 25)
+        self.active_weapon = Weapon("Chain Sword", 35)
     
     def attack(self, dinosaur):
+        # weapon = Weapon
+        # weapon.choose_weapon(self)
         dinosaur.dino_health -= self.active_weapon.weapon_attack_power
         print(f"{self.robot_name} attacks {dinosaur.dino_name} for {self.active_weapon.weapon_attack_power} damage.  Their health is now {dinosaur.dino_health}" )
--- weapon.py
+++ weapon.py
@@ -3,8 +3,15 @@ class Weapon:
         self.weapon_name = name
         self.weapon_attack_power = attack_power
 
-    # def select_weapon():
-    #     print(" Flamer"  "Boltgun"  "Chainsword")
-    #     choice = input(" Which weapon would you like to use? ")
-    #     if choice == "Flamer":
-        
+    # def choose_weapon(self):
+    #     print(" Flamer   Bolt Gun   Chain Sword")
+    #     self.choice = input("Which weapon would you like to use? ")
+    #     if self.choice == "Flamer":
+    #         self.weapon_name = "Flamer"
+    #         self.attack_power = 25
+    #     elif self.choice == "Chain Sword":
+    #         self.weapon_name = "Chain Sword"
+    #         self.attack = 35
+    #     elif self.choice == "Bolt Gun":
+    #         self.weapon_name = "Bolt Gun"
+    #         self.attack = 40
